fe0447b0cdc8620cfdf610d5f5eff7d1a930a035
Hussain-Ashknani-m1-82 <76279346+Hussain-Ashknani-m1-82@users.noreply.github.com>
2021-01-05 20:47:57 +0300

    تم حركات مو بركات

--- question1.js
+++ question1.js
@@ -1,1 +1,16 @@
 // قم بحل السؤال الأول هنا
+let pets =[`Cat`,`Dog`,`tiger`,`lion`,`bird`]
+
+pets.pop();
+
+pets.push(`dolphin`);
+
+console.log(pets);
+
+let bird = {
+    name:`pearly`,
+    age:`4 months`,
+    Function: `pearly`,
+    emoji :`🐦`
+}
+console.log(bird.age);
